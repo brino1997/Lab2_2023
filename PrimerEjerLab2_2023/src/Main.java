b8b0f41eed20a27e1a87dfd98c2e6cad6cb20c36
hzambrino97@gmail.com <hzambrino97@gmail.com>
2023-08-11 11:29:51 -0300

    modificado

--- PrimerEjerLab2_2023/src/Main.java
+++ PrimerEjerLab2_2023/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!, aburriendome en una clase de java");
     }
 }
